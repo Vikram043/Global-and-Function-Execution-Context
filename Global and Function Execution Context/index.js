cb3f85e4a31d3f1e50d73c06421aa23ea73e5da4
Vikram Naik <119391188+Vikram043@users.noreply.github.com>
2025-05-27 11:02:53 +0530

    Add files via upload

---
+++ Global and Function Execution Context/index.js
@@ -0,0 +1,18 @@
+
+let age = 25;
+
+
+function displayAge() {
+  console.log("Age is:", age);
+}
+
+
+function changeAge(newAge) {
+  age = newAge;  
+  console.log("Age changed to:", age);
+}
+
+
+displayAge();       
+changeAge(30);    
+displayAge();      
